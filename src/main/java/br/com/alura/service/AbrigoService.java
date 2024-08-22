67f7ba791598645f630193228329f0ddd08f2866
Felipe Barreto <fesacaba@gmail.com>
2024-08-22 20:19:23 -0300

    feat: separar responsabilidades

---
+++ src/main/java/br/com/alura/service/AbrigoService.java
@@ -0,0 +1,58 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttp;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class AbrigoService {
+
+    private ClientHttp http;
+
+    public AbrigoService(ClientHttp http) {
+        this.http = http;
+    }
+
+    public void cadastrarAbrigo() throws IOException, InterruptedException {
+        System.out.println("Digite o nome do abrigo:");
+        String nome = new Scanner(System.in).nextLine();
+        System.out.println("Digite o telefone do abrigo:");
+        String telefone = new Scanner(System.in).nextLine();
+        System.out.println("Digite o email do abrigo:");
+        String email = new Scanner(System.in).nextLine();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("nome", nome);
+        json.addProperty("telefone", telefone);
+        json.addProperty("email", email);
+
+        HttpResponse<String> response = http.sendPost(json, "http://localhost:8080/abrigos");
+        int statusCode = response.statusCode();
+        String responseBody = response.body();
+        if (statusCode == 200) {
+            System.out.println("Abrigo cadastrado com sucesso!");
+            System.out.println(responseBody);
+        } else if (statusCode == 400 || statusCode == 500) {
+            System.out.println("Erro ao cadastrar o abrigo:");
+            System.out.println(responseBody);
+        }
+    }
+
+    public void listarAbrigo() throws IOException, InterruptedException {
+        HttpResponse<String> response = http.sendGet("http://localhost:8080/abrigos");
+        String responseBody = response.body();
+        JsonArray jsonArray = JsonParser.parseString(responseBody).getAsJsonArray();
+        System.out.println("Abrigos cadastrados:");
+        for (JsonElement element : jsonArray) {
+            JsonObject jsonObject = element.getAsJsonObject();
+            long id = jsonObject.get("id").getAsLong();
+            String nome = jsonObject.get("nome").getAsString();
+            System.out.println(id + " - " + nome);
+        }
+    }
+}
